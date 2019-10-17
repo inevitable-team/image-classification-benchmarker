b3be9f95d787bb1cff58ca26bef1f44070d40a1e
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-10-17 14:17:27 +0100

    TensorFlow.js Example: Training MNIST

--- server/index.js
+++ server/index.js
@@ -19,4 +19,4 @@ app.use(express.static(__dirname + '/public/'));
 app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on http://localhost:${port}/ `));
